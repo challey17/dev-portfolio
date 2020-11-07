7d385ee6264efc727a23e5fc5dd03181146cefed
Chayce Knaub <chknaub@yahoo.com>
2020-11-07 15:13:55 -0500

    starting grid layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import React from "react";
 import NavBar from "./NavBar";
 import Projects from "./Projects";
 import Technologies from "./Technologies";
+import { Grid } from "@material-ui/core";
 
 const App = () => {
   return (
     <>
-      <NavBar />
-      <Technologies />
-      <Projects />
+      <Grid container direction="column">
+        <Grid item>
+          <NavBar />
+        </Grid>
+
+        {/* all body content container*/}
+
+        <Grid item container>
+          <Grid item xs={0} sm={2} />
+          <Grid item xs={12} sm={8}>
+            {/* Personal info, photo on left, blurb on right  */}
+            <Grid item container></Grid>
+            {/* Technologies */}
+            <Grid item>
+              <Technologies />
+            </Grid>
+            {/* Projects */}
+            <Grid item>
+              <Projects />
+            </Grid>
+          </Grid>
+
+          <Grid item xs={0} sm={2} />
+        </Grid>
+      </Grid>
     </>
   );
 };
